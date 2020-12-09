23540f7381c31135b1c1e89c7e4163aba12e059c
xujia.ggss.com <xujia.ggss.com>
2020-12-09 16:02:41 +0800

    push

--- src/migrations/M201029081014Message.php
+++ src/migrations/M201029081014Message.php
@@ -24,7 +24,7 @@ class M201029081014Message extends Migration
             'type' => $this->tinyInteger(2)->notNull()->defaultValue(0)->comment('消息类型 0系统消息1关注2评论3点赞4空间送花5提醒打卡6申请入群7同意入群8退群9设置管理员10移交群主权限11解散小组12移除队员13移除管理员14拒绝入群15回复16视频送花'),
             'is_read' => $this->tinyInteger(1)->notNull()->defaultValue(0)->comment('0未读1已读'),
             'target_table' => $this->string(20)->notNull()->comment('目标表'),
-            'target_id' => $this->targetId()->notNull()->comment('目标id'),
+            'target_id' => $this->integer(10)->notNull()->comment('目标id'),
             'push_type' => $this->tinyInteger(1)->notNull()->defaultValue(0)->comment('推送类型 0不推送1单推2批量推3群推'),
             'push_status' => $this->tinyInteger(1) ->notNull()->defaultValue(0)->comment('推送状态 0未推送1已推送2推送中3推送失败'),
             'created_at' => $this->integer(10)->notNull()->comment('创建时间'),
