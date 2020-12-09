de4f33899181daf78a9ab4392f0a064c3fd944b8
xujia.ggss.com <xujia.ggss.com>
2020-12-09 17:11:59 +0800

    push

--- src/migrations/M201029081014Message.php
+++ src/migrations/M201029081014Message.php
@@ -21,7 +21,7 @@ class M201029081014Message extends Migration
             'content' => $this->string(255)->defaultValue(0)->comment('消息内容'),
             'send_id' => $this->integer(11)->defaultValue(0)->comment('发送者'),
             'receive_id' => $this->integer(11)->null()->comment('接收者'),
-            'type' => $this->tinyInteger(2)->notNull()->defaultValue(0)->comment('消息类型 0系统消息1关注2评论3点赞4空间送花5提醒打卡6申请入群7同意入群8退群9设置管理员10移交群主权限11解散小组12移除队员13移除管理员14拒绝入群15回复16视频送花'),
+            'type' => $this->tinyInteger(2)->notNull()->defaultValue(0)->comment('消息类型'),
             'is_read' => $this->tinyInteger(1)->notNull()->defaultValue(0)->comment('0未读1已读'),
             'target_table' => $this->string(20)->notNull()->comment('目标表'),
             'target_id' => $this->integer(10)->notNull()->comment('目标id'),
