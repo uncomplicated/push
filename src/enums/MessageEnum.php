6756d88153f7e0838ebbaecf3f4b6f191ba5c9ba
xujia.ggss.com <xujia.ggss.com>
2020-12-10 16:21:14 +0800

    push

---
+++ src/enums/MessageEnum.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author zhangzhenjiang
+ */
+
+/**
+ * Created by .
+ * User: Administrator
+ * Date: 2020/9/3
+ * Time: 17:27
+ */
+
+
+namespace ggss\push\enums;
+
+class MessageEnum
+{
+
+
+    const MESSAGE_PUSH_TYPE_UNWANTED = 0;//不需要推送
+    const MESSAGE_PUSH_TYPE_SINGLE = 1;//单推
+    const MESSAGE_PUSH_TYPE_BATCH = 2;//批量推
+    const MESSAGE_PUSH_TYPE_TO_APP = 3;//群推
+
+    const MESSAGE_PUSH_STATUS_DEFAULT = 0 ;//未推送
+    const MESSAGE_PUSH_STATUS_SUCCESS = 1;//已推送
+    const MESSAGE_PUSH_STATUS_ONGOING = 2;//推送中
+    const MESSAGE_PUSH_STATUS_ERROR = 3;//推送失败
+}
---
+++ src/swoole/PushController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * User: xujia
+ * Date: 2020/12/8
+ */
+
+namespace console\controllers;
+
+
+use ggss\push\enums\MessageEnum;
+use ggss\push\model\Device;
+use ggss\push\model\Message;
+use ggss\push\PushClient;
+use Swoole\Process\Pool;
+use Yii;
+
+class PushController extends \yii\console\Controller
+{
+
+    public function actionRun()
+    {
+        $workerNum = 2;
+        $pool = new Pool($workerNum);
+
+        $pool->on('WorkerStart', function ($pool, $workerId){
+            echo "worker#{$workerId} is Started \n";
+            $model = Message::find()->notDeleted()->andWhere(['push_status'=>MessageEnum::MESSAGE_PUSH_STATUS_DEFAULT])->orderBy(['id' => SORT_DESC])->one();
+            if (empty($model)) {
+                return;
+            }
+            if (!Yii::$app->mutex->acquire('lock_' . $model->id, 3)) {
+                return;
+            }
+            $pushModel = new PushClient(Yii::$app->params['push']);
+            switch ($model->push_type){
+                case MessageEnum::MESSAGE_PUSH_TYPE_SINGLE :
+                case MessageEnum::MESSAGE_PUSH_TYPE_BATCH:
+                    $deviceModel = Device::find()->byUid($model->receive_id)->notDeleted()->one();
+                    if(empty($deviceModel) || empty($deviceModel->device_no) || (empty($model->title) && empty($model->content))){
+                        return false;
+                    }
+                    $res = $pushModel->pushMessageToSingle($deviceModel->device_no,$model->title,$model->content,json_encode(['route' => MessageEnum::$urls[$model->type]]));
+                    break;
+                case MessageEnum::MESSAGE_PUSH_TYPE_TO_APP:
+                    $res = $pushModel->pushMessageToApp($model->content);
+                    break;
+            }
+            if(isset($res['result'])){
+                Yii::error('推送失败id为：'.$this->id.', 推送类型:'.$model->push_type.' 错误信息：'.$res['result'],'push');
+                return false;
+            }
+
+            Yii::$app->mutex->release('lock_' . $model->id);
+        });
+
+        $pool->on('WorkerStop', function ($pool, $workerId) {
+            echo "Worker#{$workerId} is stopped\n";
+        });
+
+        $pool->start();
+    }
+
+}
